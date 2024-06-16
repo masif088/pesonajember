b359b8a3953f2ec3cd66694c30af21153185925f
masif088 <mokhamadasif@gmail.com>
2024-06-16 11:50:26 +0700

    dashboard update

--- app/Repository/View/MaterialCategory.php
+++ app/Repository/View/MaterialCategory.php
@@ -34,11 +34,18 @@ class MaterialCategory extends \App\Models\MaterialCategory implements View
 
     public static function tableData($data = null): array
     {
+        $link = route('material.category.edit',$data->id);
         return [
             ['type' => 'index'],
             ['type' => 'string', 'data' => $data->title],
             ['type' => 'string', 'data' => $data->note],
-            ['type' => 'string', 'data' => ''],
+            ['type' => 'raw_html', 'text-align' => 'center', 'data' => "
+            <div class='text-xl flex gap-1'>
+
+                <a href='$link' class='py-1 px-2 bg-secondary text-white rounded-lg'><i class='ti ti-pencil'></i></a>
+                 <a href='#' wire:click='deleteItem($data->id)' class='py-1 px-2 bg-error text-white rounded-lg'><i class='ti ti-trash'></i></a>
+            </div>
+            "],
         ];
     }
 }
