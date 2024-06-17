409f489988554a0b206676e6938d771debfea618
masif088 <mokhamadasif@gmail.com>
2024-06-17 13:28:02 +0700

    portofolio error

--- resources/views/livewire/customer-site/dashboard.blade.php
+++ resources/views/livewire/customer-site/dashboard.blade.php
@@ -116,7 +116,7 @@
                         </td>
 
                         <td class="py-2 px-2 font-extralight " style="width: 150px">
-                            {{ $c->transactionStatus->transactionStatusType->title??'' }}
+                            {{ $c->transactionStatus->transactionStatusType->title??'Belum proses' }}
                         </td>
 
                         <td class="py-2 px-2 font-extralight text-center">
