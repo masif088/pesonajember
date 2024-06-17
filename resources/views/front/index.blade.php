f38b7ca78a408ea513d69037663019b0bd7c9fd8
masif088 <mokhamadasif@gmail.com>
2024-06-18 05:30:28 +0700

    portofolio material and supplier

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png?2') }}"/>
     <style>
         body {
             margin: 0;
--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -21,7 +21,6 @@
     {{--        <link rel="stylesheet" href="{{ asset('build/assets/app-BNv0_hJj.css') }}">--}}
     {{--        <script src="{{ asset('build/assets/app-D2jpX1vH.js') }}"></script>--}}
     <link rel="stylesheet" href="{{ asset('assets/css/theme.css') }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="{{ asset('vendor/carousel/carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/select2/css/select2.css') }}">
     {{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">--}}
