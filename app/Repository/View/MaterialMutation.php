ccc8ad392098274b4328bfdf2587720e93ad4458
masif088 <mokhamadasif@gmail.com>
2024-06-17 17:10:58 +0700

    portofolio error

--- app/Repository/View/MaterialMutation.php
+++ app/Repository/View/MaterialMutation.php
@@ -28,26 +28,22 @@ class MaterialMutation extends \App\Models\MaterialMutation implements View
         return [
             ['label' => 'Tanggal', 'sort' => 'created_at'],
             ['label' => 'Keterangan', 'sort' => 'reference'],
-            ['label' => 'Perubahan Stock', 'sort' => 'amount','text-align'=>'center'],
-            ['label' => 'Stock Terkini', 'sort' => 'stock','text-align'=>'center'],
+            ['label' => 'Perubahan Stock', 'sort' => 'amount', 'text-align' => 'center'],
+            ['label' => 'Stock Terkini', 'sort' => 'stock', 'text-align' => 'center'],
         ];
     }
 
     public static function tableData($data = null): array
     {
 
-        $material= \App\Models\Material::find($data->material_id);
+        $material = \App\Models\Material::find($data->material_id);
         $linkStock = route('material.material-stock', $data->id);
-        try {
-            return [
-                ['type' => 'string' ,'data' => $data->created_at],
-                ['type' => 'raw_html', 'data' => $data->reference ."<br> <span class='text-xs'>$data->note</span>"],
-                ['type' => 'string','text-align'=>'center', 'data' => ($data->amount*$data->materialMutationStatus->operation).$material->unit],
-                ['type' => 'string','text-align'=>'center', 'data' => round($data->stock,2).$material->unit],
-            ];
-        }catch (\Exception $q){
+        return [
+            ['type' => 'string', 'data' => $data->created_at],
+            ['type' => 'raw_html', 'data' => $data->reference . "<br> <span class='text-xs'>$data->note</span>"],
+            ['type' => 'string', 'text-align' => 'center', 'data' => ($data->amount * $data->materialMutationStatus->operation) . $material->unit],
+            ['type' => 'string', 'text-align' => 'center', 'data' => round($data->stock, 2) . $material->unit],
+        ];
 
-//            dd(data$material);
-        }
     }
 }
