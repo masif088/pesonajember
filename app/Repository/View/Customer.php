38d06cf2baf5c8706c23fcee01ecb808b78007e5
masif088 <mokhamadasif@gmail.com>
2024-06-17 13:26:33 +0700

    portofolio error

--- app/Repository/View/Customer.php
+++ app/Repository/View/Customer.php
@@ -45,12 +45,12 @@ class Customer extends \App\Models\Customer implements View
             ['type' => 'raw_html', 'data' => "
             <div class='text-xl flex gap-1'>
                 <a href='$link' class='py-1 px-2 bg-secondary text-white rounded-lg'><i class='ti ti-pencil'></i></a>
-                <a href='#' onclick='clipboard()' wire:click='alert(`link customer berhasil dicopy di clipboard`)' class='py-1 px-2 bg-primary text-white rounded-lg'><i class='ti ti-link'></i></a>
+                <a href='#' onclick='clipboard(`$link2`)' wire:click='alert(`link customer berhasil dicopy di clipboard`)' class='py-1 px-2 bg-primary text-white rounded-lg'><i class='ti ti-link'></i></a>
                 <a href='#' wire:click='deleteItem($data->id)' class='py-1 px-2 bg-error text-white rounded-lg'><i class='ti ti-trash'></i></a>
             </div>
             <script>
-    function clipboard() {
-        navigator.clipboard.writeText('$link2');
+    function clipboard(text) {
+        navigator.clipboard.writeText(text);
     }
 </script>
             "],
