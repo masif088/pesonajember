bbfb0ef23692cbe40892cdea8ca936b58ad453f3
masif088 <mokhamadasif@gmail.com>
2024-06-18 05:26:45 +0700

    favicon force app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png?2') }}"/>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
