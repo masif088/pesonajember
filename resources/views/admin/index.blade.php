6bf8dc9a63f53d10899ed180b0c5c8d3659d467f
masif088 <mokhamadasif@gmail.com>
2024-06-20 12:47:03 +0700

    last update on 18 june

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -17,29 +17,29 @@
                 <div class="col-span-12 ">
                     <livewire:dashboard.dashboard-card/>
                 </div>
-                <div class="col-span-6 ">
+                <div class="lg:col-span-6 col-span-12">
                     <livewire:dashboard.dashboard-attendance/>
                 </div>
 
-                <div class="col-span-6 ">
+                <div class="lg:col-span-6 col-span-12">
                     <livewire:dashboard.dashboard-goals/>
                 </div>
-                <div class="col-span-6 ">
+                <div class="lg:col-span-6 col-span-12">
                     <livewire:dashboard.dashboard-revenue/>
                 </div>
             @elseif(auth()->user()->hasPermissionTo('dashboard-produksi', 'sanctum'))
                 <div class="col-span-12 ">
                     <livewire:dashboard.dashboard-card/>
                 </div>
-                <div class="col-span-6 ">
+                <div class="lg:col-span-6 col-span-12">
                     <livewire:dashboard.dashboard-attendance/>
                 </div>
 
-                <div class="col-span-6 ">
+                <div class="lg:col-span-6 col-span-12">
                     <livewire:dashboard.dashboard-goals/>
                 </div>
             @else
-                <div class="col-span-6 ">
+                <div class="lg:col-span-6 col-span-12">
                     <livewire:dashboard.dashboard-attendance/>
                 </div>
 
