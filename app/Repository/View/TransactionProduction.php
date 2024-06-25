16486f9c038641fe699b2a9661574b103649abb1
masif088 <mokhamadasif@gmail.com>
2024-06-25 08:03:12 +0700

    last update on 25 june

--- app/Repository/View/TransactionProduction.php
+++ app/Repository/View/TransactionProduction.php
@@ -44,9 +44,10 @@ class TransactionProduction extends Transaction implements View
         $list = "<br>";
         $listProcess = "<br>";
 //        dd($data->transactionLists);
-        foreach ($data->transactionLists as $tl ){
+        foreach ($data->transactionLists->where('edit_count',$data->edit_count) as $tl ){
+
 //            $listProcess.="$tl->uid <br>";
-            $listProcess.="<b>$tl->uid</b>: <br>";
+            $listProcess.="<b>$tl->uid</b>: ".$tl->transactionStatus->transactionStatusType->title ."<br>";
         }
 //        $link3 = route('transaction.download', $data->id);
         $link4 = route('finance.transaction.payment.detail', $data->id);
