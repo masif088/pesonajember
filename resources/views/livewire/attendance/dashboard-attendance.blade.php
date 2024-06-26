1dc9ae9c963ad625da8fce62f22b148a9cc58e0d
masif088 <mokhamadasif@gmail.com>
2024-06-26 07:07:14 +0700

    last update on 25 june

--- resources/views/livewire/attendance/dashboard-attendance.blade.php
+++ resources/views/livewire/attendance/dashboard-attendance.blade.php
@@ -16,11 +16,11 @@
         @endif
     @endif
 
-    @if(Carbon::now()->format('H:i') < "16:00" and $statusEntrance==1)
+    @if(Carbon::now()->format('H:i') < "16:00" and $statusEntrance==1 and $check->entrance_attendance_by_web!=null)
         <div>
             <a class="bg-secondary px-4 py-2 rounded-md text-white w-fit text-nowrap" href="#">
-                Anda telah melakukan presensi
-                pada {{ Carbon::parse(($check->entrance_attendance_by_web))->format('H:i') }}
+                Anda telah melakukan presensi pada
+                {{ Carbon::parse(($check->entrance_attendance_by_web))->format('H:i') }}
             </a>
 
         </div>
@@ -33,18 +33,18 @@
             </a>
         </div>
     @endif
-    @if(Carbon::now()->format('H:i') > "16:00" and $statusDischarge==1)
+    @if(Carbon::now()->format('H:i') > "16:00" and $statusDischarge==0)
         <div>
             <a href="#" wire:click="discharge()" class="px-4 py-2 bg-wishka-400 text-white text-nowrap rounded-md">
                 Klik disini untuk presensi pulang
             </a>
         </div>
     @endif
-    @if(Carbon::now()->format('H:i') > "16:00" and $statusDischarge==0)
+    @if(Carbon::now()->format('H:i') > "16:00" and $statusDischarge==1 and $check->discharge_attendance_by_web!=null)
         <div>
             <a href="#" class="bg-secondary p-4 py-2 rounded-md text-white w-fit text-nowrap">
-                Anda telah melakukan presensi pulang
-                pada {{ Carbon::parse(($check->entrance_attendance_by_web))->format('H:i') }}
+                Anda telah melakukan presensi pulang pada
+                {{ Carbon::parse($check->discharge_attendance_by_web)->format('H:i') }}
             </a>
         </div>
     @endif
